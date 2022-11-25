c1c244cf7c59aa211e7d528d4c8d9df968c213a2
Jobsidney <jobsidney67@gmail.com>
2022-11-25 22:05:29 +0300

    tailwind files setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  
+    content: [
+        "./App.{js,jsx,ts,tsx}",
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./src/pages/**/*.{js,jsx,ts,tsx}",
+        "./components/**/*.{js,jsx,ts,tsx}",
+        "./src/screens/**/*.{js,jsx,ts,tsx}"
+    
+    ],
+    }
