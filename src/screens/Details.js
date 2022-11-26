8fc8f413e329e3278690d6890a9a404f147f3a31
Jobsidney <jobsidney67@gmail.com>
2022-11-26 14:40:54 +0300

    navigation and params

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Details() {
+export default function Details({navigation}) {
   return (
     <View>
-      <Text className="items-center text-green-700">Details</Text>
+      <Text className="items-center text-green-700 underline font-bold text-2xl">{`${navigation.getParam('key')}. ${navigation.getParam('title')}`}</Text>
+      <Text>{navigation.getParam('body')}</Text>
     </View>
   )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,30 @@
-import { View, Text ,Button } from 'react-native'
-import React from 'react'
+import { View, Text ,Button,FlatList,TouchableOpacity } from 'react-native'
+import React,{useState} from 'react'
 
 export default function Home({navigation}) {
-  function pressHandler(props){
-    // navigation.navigate('Abouts')
-    navigation.push('Abouts')
+
+  const [reviews,setReviews] = useState([
+    {title:"Zelda ,Breath of fresh Air",rating:5,body:'lorem Ipsum text',key:1},
+    {title:"Zelda ,milna",rating:5,body:'lorem Ipsum text',key:4},
+    {title:"Texas new",rating:3,body:'lorem Ipsum text to the manager',key:3}
+  ])
+
+  function pressHandler(item){
+    navigation.navigate('Details',item)
+    // navigation.push('Abouts')
   }
   return (
     <View className=" text-red-900">
-      <Text className="text-red-900 font-nunito  text-xl">Home Screen</Text>
-      <Button className=" rounded-3xl bg-red-300" title="Go to reviews" onPress={pressHandler}/>
+      <FlatList
+      data={reviews}
+
+      renderItem={({item})=><TouchableOpacity className="m-3" key={item.key} onPress={()=>navigation.navigate('Details',item)}>
+        <Text className="text-orange-600 py-3 px-8 bg-blue-200 folnt-bold">{item.title}</Text>
+      </TouchableOpacity>}
+      />
+
+      {/* <Text className="text-red-900 font-nunito  text-xl">Home Screen</Text> */}
+      {/* <Button className=" rounded-3xl bg-red-300" title="Go to reviews" onPress={pressHandler}/> */}
     </View>
   )
 }
