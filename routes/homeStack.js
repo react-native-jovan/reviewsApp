fb59d4690bfc0801b62d990cbceecadc3d0b5dbb
Jobsidney <jobsidney67@gmail.com>
2022-11-26 15:05:07 +0300

    adding decoration to header

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -5,15 +5,36 @@ import Details from "../src/screens/Details";
 import About from "../src/screens/About";
 const screens={
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions:{
+            title:'GameZone',
+            headerStyle:{
+                backgroundColor:'#333',
+                
+            }
+        }
     },
     Details:{
-        screen: Details
+        screen: Details,
+        navigationOptions:{
+            title:'Details Page',
+            // headerStyle:{
+            //     backgroundColor:'gray'
+            // }
+        }
     },
     Abouts:{
         screen: About
     }
 }
-const HomeStack=createStackNavigator(screens)
+const HomeStack=createStackNavigator(screens,{
+    defaultNavigationOptions:{
+        headerTintColor:'white',
+        headerStyle:{
+            backgroundColor:'black',
+           
+        }
+    }
+})
 
 export default createAppContainer(HomeStack)
