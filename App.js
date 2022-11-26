e640afb7eb31bfa03e284953c9766d0edaf703b8
Jobsidney <jobsidney67@gmail.com>
2022-11-26 09:21:47 +0300

    adding fonts to native

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-
+import * as Font from 'expo-font'
 import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import Home from './src/screens/Home';
+
+const getFonts=()=>{
+  return Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-medium': require('./assets/fonts/Nunito-Medium.ttf'),
+  })
+}
 export default function App() {
   return (
     <TailwindProvider>   
